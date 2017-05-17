75b119ccf41c6d966312c72ce1132beac191af61
Victor Hugo <victorhugo.cweb@gmail.com>
2017-05-17 17:54:47 -0300

    ...

--- app/src/componentes/topo.jsx
+++ app/src/componentes/topo.jsx
@@ -5,19 +5,45 @@ import MenuDireita from "./menu-direita";
 
 class Topo extends Component {
     render() {
+ var iconeLeft = (
+            <div
+                onTouchTap={this.onVoltar}
+                aria-expanded="false"
+                role="button"
+                tabIndex={0}
+                className="mdl-layout__drawer-button btn-left-top">
+                <i className="material-icons">menu</i>
+            </div>
+        );
+
+
+
         return (
-            <header
-                className="mdl-layout__header">
+
+
+            <header className="mdl-layout__header ">
+                {iconeLeft}
                 <div className="mdl-layout__header-row">
-                    {/*icone menu*/}
+                    {/* this.props.title */}
                     <span className="mdl-layout-title">
-                        App GIFs React.js
+                         App GIFs React.js
                     </span>
                     <div className="mdl-layout-spacer">
                     </div>
-                    <MenuDireita />
+   
+                    <button className="mdl-button mdl-js-button mdl-button--icon">
+                         <i className="material-icons">search</i>
+                    </button>
+                     <MenuDireita />
                 </div>
+              
             </header>
+
+
+
+
+
+         
         );
     }
 };
